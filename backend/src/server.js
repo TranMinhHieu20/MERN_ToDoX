d2ecc21f800a67f1fde6364a8a1ae1c9c5099b5c
tranminhhieu <tranhieu200304@gmail.com>
2025-10-15 18:11:04 +0700

    chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,29 @@ import tasksRouters from './routes/tasksRouters.js'
 import { connectDB } from './config/db.js'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 
-const app = express()
-// middlewares
 dotenv.config()
-app.use(cors({ origin: 'http://localhost:5173' }))
 
 const PORT = process.env.PORT || 5001
 
+const __dirname = path.resolve()
+
+const app = express()
+// middlewares
 app.use(express.json())
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({ origin: 'http://localhost:5173' }))
+}
+
 app.use('/api/tasks', tasksRouters)
+
+if ((process.env.NODE_ENV = 'production')) {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.json(__dirname, '../frontend/index.html'))
+  })
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
